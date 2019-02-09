d9a83f4edc08fc25331906ec53a97218d8a3dbd6
Adrian <a.miendlarzewski@gmail.com>
2019-02-09 22:10:35 +0100

    further incremeant

--- RPG Game/RPG_Heroe.py
+++ RPG Game/RPG_Heroe.py
@@ -1,7 +1,35 @@
+import random
+
 class Heroe(object):
 
-    def __init__(self,health,armour_points,action_points,expirience):
+    def __init__(self, name, health, armour_points, action_points, expirience):
+        self.name = name
         self.health = health
-        self.armour =armour_points
+        self.armour = armour_points
         self.action_points= action_points
         self.expirience = expirience
+
+class Attack(object):
+
+    def __init__(self, basic_attack, strong_attack, knockdown=False):
+        self.basic_attack = basic_attack
+        self.strong_attack = strong_attack
+        self.knockdown = knockdown
+
+    def weak_melee(self, basic_attack):
+         damage =random.randrange(basic_attack, basic_attack+10)
+         return damage
+
+    def strong_melee(self, strong_attack):
+        damage = random.randrange(strong_attack, strong_attack+10)
+        return damage
+
+    def knockdown_attack(self, knockdown):
+        if self.knockdown == True:
+            chance = random.randrange(20)
+            if chance == 7:
+                @strong_melee
+            else:
+                pass
+        else:
+            pass
