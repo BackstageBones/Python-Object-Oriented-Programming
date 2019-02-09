20e17e17ba8d21a9c0a023a6a63ab4294957bde2
Adrian <a.miendlarzewski@gmail.com>
2019-02-09 21:34:46 +0100

    RPG game development

---
+++ RPG Game/RPG_Heroe.py
@@ -0,0 +1,7 @@
+class Heroe(object):
+
+    def __init__(self,health,armour_points,action_points,expirience):
+        self.health = health
+        self.armour =armour_points
+        self.action_points= action_points
+        self.expirience = expirience
