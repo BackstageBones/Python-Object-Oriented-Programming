2d0e3be22d141eeb8880d387a1a960bda63e06b0
BackstageBones <a.miendlarzewski@gmail.com>
2019-10-22 12:28:59 +0200

    removed RPG game from this repository

--- RPG Game/class_exceptions/class_exception.py
+++
@@ -1,8 +0,0 @@
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class MethodNotCallableForThatClass(Error):
-    """Raised when not applicable for that character"""
-    pass
--- RPG Game/game_logic.py
+++
@@ -1,5 +0,0 @@
-from villain_character_class.villain_character_class import Villain
-from character_abstractclass.character_constants import CharacterConstant
-
-Warboss = [CharacterConstant.basic_damage['force_commander'], CharacterConstant.strong_damage['force_commander']].
-Gorgutz = Villain.from_string(Warboss)
