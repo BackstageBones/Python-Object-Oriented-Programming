55dee5390794e91c30caf5fc38c1d63ad2d0da7b
Adrian <a.miendlarzewski@gmail.com>
2019-11-06 17:24:35 +0100

    added function that suggest closest word using machining algorithm from python builtin library.

--- Interactive_dictionary/interactive_dictionary.py
+++ Interactive_dictionary/interactive_dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches, SequenceMatcher
 
 
 class InteractiveDictionary(object):
@@ -17,9 +18,22 @@ class InteractiveDictionary(object):
         try:
             self.dictionary[question]
         except KeyError:
-            print("No word definition found for that key, try again")
+            print(self.calculate_matches(question))
         else:
             print(self.dictionary[question])
+
+    def calculate_matches(self, question):
+        calculate_match = get_close_matches(question, self.dictionary.keys(), n=1)
+        match_ratio = SequenceMatcher(None, question, calculate_match[0])
+        if match_ratio.quick_ratio() > 0.8:
+            print("Did you mean :", calculate_match, '?')
+            user_input = input("Y/N ?").lower()
+            if user_input in ('yes', 'y'):
+                return self.dictionary[calculate_match[0]]
+            else:
+                pass
+        else:
+            return "No word definition found for that key, try again"
 
     def interface(self):
         while True:
