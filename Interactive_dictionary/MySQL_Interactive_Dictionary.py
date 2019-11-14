c9cacae1abafe354b8805f4a1d4f5b511d58d436
BackstageBones <a.miendlarzewski@gmail.com>
2019-11-14 21:23:49 +0100

    created alternate version of dictionary which is based on a remote SQL database

---
+++ Interactive_dictionary/MySQL_Interactive_Dictionary.py
@@ -0,0 +1,64 @@
+from difflib import get_close_matches
+
+import mysql.connector
+
+
+class MySqlInteractiveDictionary(object):
+    def __init__(self):
+        self._connection = mysql.connector.connect(
+            user="ardit700_student",
+            password="ardit700_student",
+            host="108.167.140.122",
+            database="ardit700_pm1database"
+        )
+
+        self._cursor = self._connection.cursor(())
+        self.close_words = ('q', 'quit', 'end', '/')
+
+    def ask_for_key(self):
+        while True:
+            question = (input("type the key: "))
+            if not question.isalpha():
+                return "You have to provide a key word not a number"
+            return question
+
+    def search_data(self, question):
+        question_variants = [question.lower(), question.upper(), question.title()]
+        query = "SELECT * FROM Dictionary WHERE Expression = '{}'"
+
+        for variants in question_variants:
+            self._cursor.execute(query.format(variants))
+            results = self._cursor.fetchall()
+            if results:
+                return [result for result in results]
+            else:
+                self.calculate_matches(question)
+                break
+
+
+    def calculate_matches(self, question):
+        self._cursor.execute("SELECT * FROM Dictionary")
+        results = self._cursor.fetchall()
+        matches = [result for result in results]
+
+        calculate_match = get_close_matches(question,matches)
+        print("Did you mean :", calculate_match, '?')
+        user_input = input("Y/N ?").lower()
+        if user_input in ('yes', 'y'):
+            return calculate_match
+        else:
+            return "No word definition found for that key, try again"
+
+
+    def interface(self):
+        while True:
+            question = self.ask_for_key()
+            if question in self.close_words:
+                break
+            else:
+                print(self.search_data(question))
+
+
+if __name__ == "__main__":
+    dictionary = MySqlInteractiveDictionary()
+    print(dictionary.interface())
--- Sentence_maker_programme/sentence_maker_programme.py
+++ Sentence_maker_programme/sentence_maker_programme.py
@@ -1,8 +1,8 @@
 class MakeSentence(object):
 
     def __init__(self):
-        self.simple_list = []
-        self.interrogatives = ('who', 'where', 'why', 'how')
+        self._simple_list = []
+        self._interrogatives = ('who', 'where', 'why', 'how')
 
     def ask_for_sentence(self):
         for i in range(0, 3):
@@ -14,15 +14,15 @@ class MakeSentence(object):
                 if question == '/end':
                     break
                 else:
-                    self.simple_list.append(question)
+                    self._simple_list.append(question)
 
     def format_sentence(self):
-        for word in self.simple_list:
-            if word.startswith(self.interrogatives):
-                self.simple_list[self.simple_list.index(word)] = word + " ?"
+        for word in self._simple_list:
+            if word.startswith(self._interrogatives):
+                self._simple_list[self._simple_list.index(word)] = word + " ?"
             else:
-                self.simple_list[self.simple_list.index(word)] = word + "."
-        print(" ".join(self.simple_list))
+                self._simple_list[self._simple_list.index(word)] = word + "."
+        print(" ".join(self._simple_list))
 
 
 if __name__ == "__main__":
