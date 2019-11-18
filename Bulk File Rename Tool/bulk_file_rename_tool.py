25e3567fff9f290d7f1960437e4b3566d5743d1d
BackstageBones <a.miendlarzewski@gmail.com>
2019-11-18 19:32:17 +0100

    major fixes inside reanming tool

--- Bulk File Rename Tool/bulk_file_rename_tool.py
+++ Bulk File Rename Tool/bulk_file_rename_tool.py
@@ -1,11 +1,13 @@
 import os
 import shutil
+import filetype
 
 
 class FileRenameTool(object):
     def __init__(self):
         self._os = os
         self._shutil = shutil
+        self._filetype = filetype
 
     def _print_current_directory(self):
         print("Your current working directory is: \n {}".format(self._os.getcwd()))
@@ -22,37 +24,63 @@ class FileRenameTool(object):
         finally:
             print(self._os.listdir(self._os.getcwd()))
 
+    def rename_files_in_folder(self, ):
+        images_extensions = ('.jpg', '.png', '.psd', '.tiff',)
+        documents_extensions = ('.doc', '.txt', '.pdf')
+        # ask which files would you like to rename
+        user_specify = input(
+            "Which files would you like to rename? \n type 'images' for images or 'text for text files")
+        if user_specify in ('images', 'pictures'):
+            user_naming = input("Provide file naming convention")
+            i = 0
+            for file in self._os.listdir(self._os.getcwd()):
+                i += 1
+                if self._os.path.isfile(file):
+                # name, ext = self._os.path.splitext(file)
+                    kind = self._filetype.guess(self._os.path.abspath(file))
+                    print(kind)
 
-    def rename_files_in_folder(self,):
+    """        
+     elif user_specify in ('text', 'documents'):
+         user_input = input("Provide file naming convention")
+         i = 0
+         for file in self._os.listdir(self._os.getcwd()):
+             i += 1
+             name, ext = self._os.path.splitext(file)
+             if ext == documents_extensions[0]:
+                 self._os.rename(file, user_input + '_' + str(i) + documents_extensions[0])
+             elif ext == documents_extensions[1]:
+                 self._os.rename(file, user_input + '_' + str(i) + documents_extensions[1])
+             elif ext == documents_extensions[2]:
+                 self._os.rename(file, user_input + '_' + str(i) + documents_extensions[2])
+             else:
+                 break
+     else:
+         return "unknown file type, cannot rename"
+ """
 
-        user_input = input("Provide file naming convention")
-        for file in self._os.listdir(self._os.getcwd()):
-            for i in range(len(self._os.listdir(self._os.getcwd()))):
-                self._os.rename(file, user_input + '_' + str(i))
-
-    def make_copy(self,):
+    def make_copy(self):
+        copy_place = self._os.getcwd() + "//copied_old"
         try:
-            self._os.mkdir(self._os.getcwd()+"copied_old")
+            self._os.mkdir(copy_place)
         except OSError:
             print("Folder cannot be created")
-
-        for file in self._os.listdir(self._os.getcwd()):
-            try:
-                self._shutil.copy(self._os.getcwd(), "copied_old")
-            except shutil.SameFileError:
-                print("Source and destination represents the same file.")
-            except PermissionError:
-                print("Permission denied.")
-
-
-
-
+        else:
+            for file_name in self._os.listdir(self._os.getcwd()):
+                full_file_name = self._os.path.join(self._os.getcwd(), file_name)
+                if self._os.path.isfile(full_file_name):
+                    try:
+                        self._shutil.copy(file_name, copy_place)
+                    except shutil.SameFileError:
+                        print("Source and destination represents the same file.")
+                    except PermissionError:
+                        print("Permission denied.")
+        finally:
+            print("Copy made")
 
 
 if __name__ == "__main__":
     BulkTool = FileRenameTool()
     BulkTool.change_directory()
     BulkTool.make_copy()
-    BulkTool._print_current_directory()
     BulkTool.rename_files_in_folder()
-
