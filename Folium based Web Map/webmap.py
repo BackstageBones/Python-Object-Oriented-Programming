b68d3aeb37aee1b7fcdb0a7a8424b2648d6e21b4
BackstageBones <a.miendlarzewski@gmail.com>
2019-11-24 00:50:58 +0100

    branched webmap class

---
+++ Folium based Web Map/webmap.py
@@ -0,0 +1,7 @@
+import folium
+from .webmap_constants import WebMapConstants
+
+
+class FoliumWebMap(folium):
+    def __init__(self):
+        self._folium = foliumgit
---
+++ Folium based Web Map/webmap_constants.py
@@ -0,0 +1,3 @@
+class WebMapConstants(object):
+
+    localizations = {'Wroclaw': (51.1099,17.0334), 'Chicago': (41.8829,-87.7787)}
