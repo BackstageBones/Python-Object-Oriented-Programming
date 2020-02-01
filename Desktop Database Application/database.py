82318f605435620fc4ad27d25cb733336050bf33
BackstageBones <a.miendlarzewski@gmail.com>
2020-02-01 18:18:12 +0100

    fucntion to fill tables

--- Desktop Database Application/database.py
+++ Desktop Database Application/database.py
@@ -16,14 +16,29 @@ class MyDataBase(object):
         return self.connect.close()
 
     def create_table(self, **kwargs):
-        query = 'CREATE TABLE IF NOT EXISTS {} ('
-        comma = r','
+        query = "CREATE TABLE IF NOT EXISTS {} ("
+        comma = r', '
         for key, value in kwargs.items():
             if key != 'table_name':
                 query = query + key + ' ' + value + comma
-        query = query.format(kwargs['table_name']).rstrip(',') + ")"
+        query = query.format(kwargs['table_name']).rstrip(' ,') + ")"
+        print(query)
+        return self.cursor.execute(query)
+
+    def insert_values(self, *args):
+        query = r"INSERT INTO {} VALUES ("
+        comma = r','
+        for arg in args:
+            if arg != args[0]:
+                if type(arg) == str:
+                    query = query + "'" + arg + "'" + comma
+                else:
+                    query = query + str(arg) + comma
+        query = query.format(args[0]).rstrip(" ,") + ")"
+        print(query)
         return self.cursor.execute(query)
 
 
 db = MyDataBase('local')
 db.create_table(table_name='store', item='TEXT', quantity='INTEGER', price='REAL')
+#db.insert_values('store', 'wine glass', 8, 10.5)
