587fe283239d5938ad913099c4007f6117458117
BackstageBones <a.miendlarzewski@gmail.com>
2020-02-06 14:32:56 +0100

    database refinement

--- Desktop Database Application/database.py
+++ Desktop Database Application/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 
 class MyDataBase(object):
@@ -16,6 +17,10 @@ class MyDataBase(object):
         return self.connect.close()
 
     def create_table(self, **kwargs):
+        """
+         param kwargs: create arbitrary number of columns inside your table
+         param table_name: first parameter must be specified with table_name which creates table
+        """
         query = "CREATE TABLE IF NOT EXISTS {} ("
         comma = r', '
         for key, value in kwargs.items():
@@ -23,6 +28,7 @@ class MyDataBase(object):
                 query = query + key + ' ' + value + comma
         query = query.format(kwargs['table_name']).rstrip(' ,') + ")"
         return self.cursor.execute(query)
+
 
     def insert_values(self, *args):
         query = r"INSERT INTO {} VALUES ("
@@ -36,13 +42,35 @@ class MyDataBase(object):
         query = query.format(args[0]).rstrip(" ,") + ")"
         return self.cursor.execute(query)
 
+
     def view(self, table_name):
         self.cursor.execute("SELECT * FROM {}".format(table_name))
         return self.cursor.fetchall()
+
+
+    def delete_specific_row(self, table_name, row, item):
+        query = "DELETE FROM {} WHERE {}='{}'"
+        query = query.format(table_name, row, item)
+        self.cursor.execute(query)
+
+    def update_table(self, **kwargs):
+        query = "UPDATE {} SET "
+        query_ending = " WHERE {}={}"
+        comma = r', '
+        for key, value in kwargs.items():
+            if key != 'table_name':
+                if key != 'item':
+                    query = query + str(key) + '=' + str(value) + comma
+                else:
+                    query = query.format(kwargs['table_name']).rstrip(" ,") + query_ending.format(key,"'" + value+"'")
+        return self.cursor.execute(query)
+
 
 
 
 db = MyDataBase('local')
 db.create_table(table_name='store', item='TEXT', quantity='INTEGER', price='REAL')
 db.insert_values('store', 'wine glass', 8, 10.5)
-db.view('store')
+#db.delete_specific_row('store','item', "wine glass")
+db.update_table(table_name='store', quantity=10, price=15, item='coffe cup')
+print(db.view('store'))
