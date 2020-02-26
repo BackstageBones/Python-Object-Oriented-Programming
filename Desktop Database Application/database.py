3c5b97f5ac8d03b5b7ec32f4a688890e574def6f
BackstageBones <a.miendlarzewski@gmail.com>
2020-02-26 14:48:54 +0100

    finished app

--- Desktop Database Application/database.py
+++ Desktop Database Application/database.py
@@ -4,7 +4,6 @@ class creates a local db file at its instantiation and allows to insert, update 
 """
 
 import sqlite3
-import pdb
 
 
 class MyDataBase(object):
@@ -18,6 +17,9 @@ class MyDataBase(object):
     def __str__(self):
         return MyDataBase
 
+    def __del__(self):
+        self.close_connection()
+
     @property
     def database_name(self):
         return self.db_name
@@ -26,16 +28,18 @@ class MyDataBase(object):
         return self.connect.commit()
 
     def close_connection(self):
+        self.connect.commit()
         return self.connect.close()
 
     def create_table(self):
-        query = """CREATE TABLE IF NOT EXISTS book ("id" INTEGER PRIMARY KEY, "title" TEXT, "author" TEXT, 
-        "year" INTEGER,"isbn" INTEGER) """
+        query = "CREATE TABLE IF NOT EXISTS book ('id' INTEGER PRIMARY KEY, 'title' TEXT, 'author' TEXT,year INTEGER, isbn INTEGER)"
         self.cursor.execute(query)
         return self.commit_changes()
 
     def insert_values(self, title, author, year, isbn):
-        query = f"INSERT INTO book (id, title, author, year, isbn) VALUES (NULL, '{title}', '{author}', '{year}', '{isbn}')"
+        query = "INSERT INTO book (id, title, author, year, isbn) VALUES (NULL, '{}', '{}', {}, {})".format(title,
+                                                                                                        author,
+                                                                                                        year, isbn)
         self.cursor.execute(query)
         return self.commit_changes()
 
@@ -44,25 +48,22 @@ class MyDataBase(object):
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
-    def delete_specific_row(self, row, item):
-        query = "DELETE FROM book WHERE {}='{}'"
-        query = query.format(row, item)
+    def delete_specific_row(self, id):
+        query = "DELETE FROM book WHERE id={}".format(id)
         self.cursor.execute(query)
         return self.commit_changes()
 
-    def update_table(self, title, author, year, isbn, id):
+    def update_table(self, id, title, author, year, isbn):
         query = f"UPDATE book SET title={title}, author={author}, year={year}, isbn={isbn} WHERE id={id}"
         self.cursor.execute(query)
         return self.commit_changes()
 
     def search_table(self, title, author, year, isbn):
-        query = f"SELECT * FROM book WHERE title ='{title}' OR author='{author}' OR year='{year}' OR isbn='{isbn}'"
+        query = f"SELECT * FROM book WHERE title='{title}' OR author='{author}' OR year='{year}' OR isbn='{isbn}'"
         self.cursor.execute(query)
         return self.cursor.fetchall()
-
 
 # if __name__ == "__main__":
 #     mydatabase = MyDataBase('local.db')
 #     mydatabase.insert_values('sapiens', 'harrari', 2012, 1227)
 #     print(mydatabase.search_table('sapiens'))
-
--- Desktop Database Application/gui.py
+++ Desktop Database Application/gui.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label, Entry, StringVar, Listbox, Scrollbar, Button
+
 from database import MyDataBase
 
 
@@ -7,6 +8,7 @@ class Database_GUI(MyDataBase):
     def __init__(self, db_name):
         self.db_name = db_name
         self.window = Tk()
+        self.window.title('Book Management App')
         super().__init__(db_name)
 
         self.title_label = Label(self.window, text="Title")
@@ -39,29 +41,30 @@ class Database_GUI(MyDataBase):
 
         self.listbox = Listbox(self.window, height=6, width=45)
         self.listbox.grid(row=2, column=0, columnspan=6, rowspan=6)
+        self.listbox.bind('<<ListboxSelect>>', self.get_selected_row)
 
         self.scrollbar = Scrollbar(self.window)
         self.scrollbar.grid(row=2, column=6, rowspan=6)
 
         self.listbox.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar.configure(command=self.listbox.yview)
-        
+
         self.button_view_all = Button(self.window, text="View all", width=12, command=self.view_command)
         self.button_view_all.grid(row=0, column=7)
 
         self.button_search_entry = Button(self.window, text="Search entry", width=12, command=self.search_command)
         self.button_search_entry.grid(row=1, column=7)
 
-        self.button_add_entry = Button(self.window, text="Add entry", width=12)
+        self.button_add_entry = Button(self.window, text="Add entry", width=12, command=self.insert_command)
         self.button_add_entry.grid(row=2, column=7)
 
         self.button_update = Button(self.window, text="Update", width=12)
         self.button_update.grid(row=3, column=7)
 
-        self.button_delete = Button(self.window, text="Delete", width=12)
+        self.button_delete = Button(self.window, text="Delete", width=12, command=self.delete_command)
         self.button_delete.grid(row=4, column=7)
 
-        self.button_close = Button(self.window, text="Close", width=12)
+        self.button_close = Button(self.window, text="Close", width=12, command=self.close_command)
         self.button_close.grid(row=5, column=7)
 
     def view_command(self):
@@ -71,11 +74,43 @@ class Database_GUI(MyDataBase):
 
     def search_command(self):
         self.listbox.delete(0, 'end')
-        for row in self.search_table(self.title_entry.get(), self.author_entry.get(), self.year_entry.get(), self.isbn_entry.get()):
+        for row in self.search_table(self.title_entry.get(), self.author_entry.get(), self.year_entry.get(),
+                                     self.isbn_entry.get()):
             self.listbox.insert('end', row)
+
+    def insert_command(self):
+        self.insert_values(self.title_entry.get(), self.author_entry.get(), self.year_entry.get(),
+                           self.isbn_entry.get())
+        return self.listbox.insert('end', (
+            self.title_entry.get(), self.author_entry.get(), self.year_entry.get(), self.isbn_entry.get()))
+
+    def delete_command(self):
+        self.listbox.delete(0, 'end')
+        return self.delete_specific_row(self.index[0])
+
+    def close_command(self):
+        return self.window.destroy()
+
+    def get_selected_row(self, event):
+        try:
+            self.index = self.listbox.get(self.listbox.curselection()[0])
+            self.title_text_entry.delete(0, 'end')
+            self.title_text_entry.insert('end', self.index[1])
+            self.author_text_entry.delete(0, 'end')
+            self.author_text_entry.insert('end', self.index[2])
+            self.year_text_entry.delete(0, 'end')
+            self.year_text_entry.insert('end', self.index[3])
+            self.isbn_text_entry.delete(0, 'end')
+            self.isbn_text_entry.insert('end', self.index[4])
+        except IndexError:
+            pass
+
+    def update_command(self):
+        self.update_table(self.index[0], self.title_entry.get(), self.author_entry.get(), self.year_entry.get(),
+                          self.isbn_entry.get())
 
 
 if __name__ == "__main__":
     window = Database_GUI('local.db')
-    window.insert_values('sapiens', 'harrari', 2012, 1227)
+    # window.insert_values('sapiens', 'harrari', 2012, 1227)
     window.window.mainloop()
